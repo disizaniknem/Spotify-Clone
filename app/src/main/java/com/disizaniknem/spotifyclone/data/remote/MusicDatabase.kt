f8e9c15967b11297cd44b20878382ba858ee40f3
disizaniknem <disizaniknem@gmail.com>
2021-02-07 17:47:48 +0100

    Setup Firebase Firestore

---
+++ app/src/main/java/com/disizaniknem/spotifyclone/data/remote/MusicDatabase.kt
@@ -0,0 +1,21 @@
+package com.disizaniknem.spotifyclone.data.remote
+
+import com.disizaniknem.spotifyclone.data.entities.Song
+import com.disizaniknem.spotifyclone.other.Constants.SONG_COLLECTION
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
+
+class MusicDatabase {
+
+    private val firestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs() : List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+}
