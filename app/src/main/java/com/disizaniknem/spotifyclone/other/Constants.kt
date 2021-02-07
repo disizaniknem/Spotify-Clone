a7df2534cdc7ee28a286b49514a6b6a4c949137f
disizaniknem <disizaniknem@gmail.com>
2021-02-07 21:42:29 +0100

    Notification

--- app/src/main/java/com/disizaniknem/spotifyclone/other/Constants.kt
+++ app/src/main/java/com/disizaniknem/spotifyclone/other/Constants.kt
@@ -3,4 +3,8 @@ package com.disizaniknem.spotifyclone.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
+
 }
