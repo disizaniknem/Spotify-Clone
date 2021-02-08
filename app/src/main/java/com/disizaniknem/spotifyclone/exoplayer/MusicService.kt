8be7961a77d6431210d56bc74b30c3de2ef39fb8
disizaniknem <disizaniknem@gmail.com>
2021-02-08 22:39:41 +0100

    Connecteur Service / Ui

--- app/src/main/java/com/disizaniknem/spotifyclone/exoplayer/MusicService.kt
+++ app/src/main/java/com/disizaniknem/spotifyclone/exoplayer/MusicService.kt
@@ -12,6 +12,7 @@ import com.disizaniknem.spotifyclone.exoplayer.callbacks.MusicPlaybackPreparer
 import com.disizaniknem.spotifyclone.exoplayer.callbacks.MusicPlayerEventListener
 import com.disizaniknem.spotifyclone.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.disizaniknem.spotifyclone.other.Constants.MEDIA_ROOT_ID
+import com.disizaniknem.spotifyclone.other.Constants.NETWORK_ERROR
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
@@ -148,6 +149,7 @@ class MusicService : MediaBrowserServiceCompat() {
                             isPlayerInitialized = true
                         }
                     } else {
+                        mediaSession.sendSessionEvent(NETWORK_ERROR, null)
                         result.sendResult(null)
                     }
                 }
