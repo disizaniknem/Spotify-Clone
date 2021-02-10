c0d65558d0a06403cdc46fb89523659317cdda7b
disizaniknem <disizaniknem@gmail.com>
2021-02-10 21:06:33 +0100

    Ajout du view model pour une musique afin que la seekbar progresse

---
+++ app/src/main/java/com/disizaniknem/spotifyclone/ui/viewmodels/SongViewModel.kt
@@ -0,0 +1,43 @@
+package com.disizaniknem.spotifyclone.ui.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.disizaniknem.spotifyclone.exoplayer.MusicService
+import com.disizaniknem.spotifyclone.exoplayer.MusicServiceConnection
+import com.disizaniknem.spotifyclone.exoplayer.currentPlaybackPosition
+import com.disizaniknem.spotifyclone.other.Constants.UPDATE_PLAYER_POSITION_INTERVAL
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SongViewModel @ViewModelInject constructor(
+    musicServiceConnection: MusicServiceConnection
+) : ViewModel() {
+
+    private val playbackState = musicServiceConnection.playbackState
+
+    private val _curSongDuration = MutableLiveData<Long>()
+    val curSongDuration: LiveData<Long> = _curSongDuration
+
+    private val _curPlayerPosition = MutableLiveData<Long>()
+    val curPlayerPosition: LiveData<Long> = _curPlayerPosition
+
+    init {
+        updateCurrentPlayerPosition()
+    }
+
+    private fun updateCurrentPlayerPosition() {
+        viewModelScope.launch {
+            while (true) {
+                val position = playbackState.value?.currentPlaybackPosition
+                if (curPlayerPosition.value != position) {
+                    _curPlayerPosition.postValue(position)
+                    _curSongDuration.postValue(MusicService.curSongDuration)
+                }
+                delay(UPDATE_PLAYER_POSITION_INTERVAL)
+            }
+        }
+    }
+}
